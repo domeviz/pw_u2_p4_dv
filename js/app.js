6bc854af518d3cbd57e3d05de829b5c83687ffdd
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:44:36 -0500

    Taller 14

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 console.log("Componente Vue")
 console.log(Vue)
 
-const app=Vue.createApp({
-    template:`
-    <h1>Hola Mundo</h1>
-    <p>Desde Vue.JS</>
-    `
+//Options API
+const app = Vue.createApp({
+    //template:`
+    //<h1>Hola Mundo</h1>
+    //<p>Desde Vue.JS</>
+    //<p>{{1+1}}</p>
+    //`
+    data(){
+        return{
+            nombre: 'Domenica',
+            apellido: 'Vizcarra',
+            mensaje: 'Hola mundo 2 desde Veu.JS'
+        }
+    },
+    methods:{
+        cambiarNombre(){
+            console.log('Boton para cambiar nombre')
+            this.nombre='Daniel'
+        },
+        cambiarApellido(){
+            console.log('Boton para cambiar apellido')
+            this.apellido='Teran'
+        }
+    }
 })
 
 app.mount('#myApp')

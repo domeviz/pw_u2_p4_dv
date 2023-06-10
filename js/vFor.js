0a74401ada10481bc1ec0682251b0eff43c870db
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-10 10:48:50 -0500

    Taller 15

--- js/vFor.js
+++ js/vFor.js
@@ -1,5 +1,64 @@
+const estudiante = {
+    nombre: 'Domenica',
+    apellido: 'Vizcarra'
+}
+const arreglo = [
+    {nombre: 'Domenica',apellido: 'Vizcarra'},
+    {nombre: 'Alison',apellido: 'Vizcarra'},
+    {nombre: 'Antony',apellido: 'Arguello'},
+    {nombre: 'Juan',apellido: 'Jimenez'},
+    {nombre: 'Josue',apellido: 'Masabanda'},
+    {nombre: 'Fernando',apellido: 'Mosquera'},
+    {nombre: 'Jessica',apellido: 'Yanez'},
+    {nombre: 'Axel',apellido: 'Flores'},
+    {nombre: 'Mike',apellido: 'Garcia'},
+    {nombre: 'Paulo',apellido: 'Aldaz'}
+]
+
+console.log(arreglo)
 const app = Vue.createApp({
-   
+    data(){
+        return{
+            miarreglo: arreglo,
+            nombre:'Dome'
+        }
+    },
+    methods:{
+        agregarEstudiante(event){
+            console.log('Vamos a agregar')
+            const nuevoEstudiante={
+                nombre:this.nombre,
+                apellido: 'Nuevo Apellido'
+            }
+            //push al final
+            //unshift al principio
+            this.miarreglo.unshift(nuevoEstudiante)
+        },
+        agregarApellido(event){
+            console.log('Vamos a agregar')
+            console.log(this.nombre)
+            console.log(event)
+            console.log(event.charCode)
+            if (event.charCode=='13'){
+                console.log('presiono enter')
+                const nuevoEstudiante={
+                    nombre:this.nombre,
+                    apellido: 'Nuevo Apellido'
+                }
+                this.miarreglo.push(nuevoEstudiante)
+            }else{
+                console.log('no presiono enter')
+            }
+        }
+    }
+
 })
 
 app.mount('#myApp2')
+
+function insertarEstudiante(estudiante){
+
+}
+function insertarEstudiante(nombre,apellido){
+
+}
